c8e80073c8a54a5880d38c3715e31635cdc26e25
BDUREAU <boris.dureau@neuf.fr>
2019-07-02 07:53:35 +0200

    Added missing files

---
+++ app/src/main/java/com/motorgimbalconsole/FlightListActivity.java
@@ -0,0 +1,175 @@
+//package com.altimeter.bdureau.bearconsole;
+package com.motorgimbalconsole;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+/**
+ *   @description: This retrieve the flight list
+ *   @author: boris.dureau@neuf.fr
+ **/
+public class FlightListActivity extends AppCompatActivity {
+    public static String SELECTED_FLIGHT = "MyFlight";
+
+    ListView flightList=null;
+    ConsoleApplication myBT;
+    List<String> flightNames = null;
+    private FlightData myflight = null;
+    private ProgressDialog progress;
+
+    private Button buttonDismiss;
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+            // Get the flight name
+            String currentFlight = ((TextView) v).getText().toString();
+
+            // Make an intent to start next activity.
+            Intent i = new Intent(FlightListActivity.this, FlightViewActivity.class);
+
+            //Change the activity.
+            i.putExtra(SELECTED_FLIGHT, currentFlight);
+            startActivity(i);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //get the bluetooth Application pointer
+        myBT = (ConsoleApplication) getApplication();
+        //Check the local and force it if needed
+        getApplicationContext().getResources().updateConfiguration(myBT.getAppLocal(), null);
+
+        setContentView(R.layout.activity_flight_list);
+        buttonDismiss =  (Button) findViewById(R.id.butDismiss);
+        new RetrieveFlights().execute();
+        buttonDismiss.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();      //exit the activity
+            }
+        });
+    }
+
+    private void getFlights() {
+
+        //get flights
+        if (myBT.getConnected()) {
+                //clear anything on the connection
+                myBT.flush();
+                myBT.clearInput();
+                //myBT.getFlightData().ClearFlight();
+                // Send command to retrieve the config
+                myBT.write("a;\n".toString());
+                myBT.flush();
+
+        try {
+            //wait for data to arrive
+            while (myBT.getInputStream().available() <= 0) ;
+        } catch (IOException e) {
+           // msg("Failed to retrieve flights");
+        }
+       // if (myBT.getConnected()) {
+            String myMessage = "";
+            myBT.setDataReady(false);
+           // myBT.initFlightData();
+            myMessage = myBT.ReadResult(60000);
+
+            if (myMessage.equals("start end")) {
+
+                flightNames = new ArrayList<String>();
+
+                //myflight = myBT.getFlightData();
+                flightNames = myflight.getAllFlightNames2();
+            }
+        }
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+
+    private class RetrieveFlights extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private  AlertDialog.Builder builder=null;
+        private AlertDialog alert;
+        @Override
+        protected void onPreExecute()
+        {
+            //"Retrieving flights..."
+            //"Please wait!!!"
+           /* progress = ProgressDialog.show(FlightListActivity.this,
+                    getResources().getString(R.string.msg7),
+                    getResources().getString(R.string.msg8));  //show a progress dialog
+*/
+
+            builder = new AlertDialog.Builder(FlightListActivity.this);
+            //Retrieving flights...
+            builder.setMessage(getResources().getString(R.string.msg7))
+                    .setTitle(getResources().getString(R.string.msg8))
+                    .setCancelable(false)
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog, final int id) {
+                            dialog.cancel();
+                            myBT.setExit(true);
+                        }
+                    });
+            alert = builder.create();
+            alert.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+
+            getFlights();
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            final ArrayAdapter adapter = new ArrayAdapter(FlightListActivity.this, android.R.layout.simple_list_item_1, flightNames);
+            adapter.sort(new Comparator<String>() {
+                public int compare(String object1, String object2) {
+                    return object1.compareTo(object2);
+                }
+            });
+
+            flightList = (ListView) findViewById(R.id.listViewFlightList);
+
+
+                flightList.setAdapter(adapter);
+            flightList.setOnItemClickListener(myListClickListener);
+
+            //progress.dismiss();
+            alert.dismiss();
+            if (myflight.getNbrOfFlight()==0 )
+                msg("No flights have been recorded");
+        }
+    }
+
+}
---
+++ app/src/main/java/com/motorgimbalconsole/GimbalConfigData.java
@@ -0,0 +1,225 @@
+package com.motorgimbalconsole;
+
+/**
+ *
+ *   @description: Gimbal configuration
+ *   @author: boris.dureau@neuf.fr
+ *
+ **/
+public class GimbalConfigData {
+
+    //Gimbal config variables
+    private String AltimeterName = "RocketGimbal";
+    private long ax_offset =0;
+    private long ay_offset =0;
+    private long az_offset =0;
+    private long gx_offset =0;
+    private long gy_offset =0;
+    private long gz_offset =0;
+    private double KpX= 0.0;
+    private double KiX= 0.0;
+    private double KdX= 0.0;
+    private double KpY= 0.0;
+    private double KiY= 0.0;
+    private double KdY= 0.0;
+    private int servoXMin = 0;
+    private int servoXMax=120;
+    private int servoYMin = 0;
+    private int servoYMax=120;
+    private long connectionSpeed= 38400;
+    private int altimeterResolution = 0;
+    private int eepromSize= 512;
+    private int minorVersion =0;
+    private int majorVersion =0;
+    private int units = 0;
+    private int endRecordAltitude =3;
+    private int beepingFrequency = 440;
+
+    public GimbalConfigData()
+    {
+
+    }
+
+    public void setAltimeterName(String value)
+    {
+        AltimeterName = value;
+    }
+    public String getAltimeterName()
+    {
+        return AltimeterName;
+    }
+
+    public void setAxOffset(long value) {ax_offset = value;}
+    public long getAxOffset()
+    {
+        return ax_offset;
+    }
+
+    public void setAyOffset(long value) {ay_offset = value;}
+    public long getAyOffset()
+    {
+        return ay_offset;
+    }
+
+    public void setAzOffset(long value) {az_offset = value;}
+    public long getAzOffset()
+    {
+        return az_offset;
+    }
+
+    public void setGxOffset(long value) {gx_offset = value;}
+    public long getGxOffset()
+    {
+        return gx_offset;
+    }
+
+    public void setGyOffset(long value) {gy_offset = value;}
+    public long getGyOffset()
+    {
+        return gy_offset;
+    }
+
+    public void setGzOffset(long value) {gz_offset = value;}
+    public long getGzOffset()
+    {
+        return gz_offset;
+    }
+
+    public void setKpX(double value) {KpX =value;}
+    public double getKpX()
+    {
+        return KpX;
+    }
+
+    public void setKiX(double value) {KiX =value;}
+    public double getKiX()
+    {
+        return KiX;
+    }
+
+    public void setKdX(double value) {KdX =value;}
+    public double getKdX()
+    {
+        return KdX;
+    }
+
+    public void setKpY(double value) {KpY =value;}
+    public double getKpY()
+    {
+        return KpY;
+    }
+
+    public void setKiY(double value) {KiY =value;}
+    public double getKiY()
+    {
+        return KiY;
+    }
+
+    public void setKdY(double value) {KdY =value;}
+    public double getKdY()
+    {
+        return KdY;
+    }
+    public int getAltiMinorVersion()
+    {
+        return minorVersion;
+    }
+    public int getAltiMajorVersion()
+    {
+        return majorVersion;
+    }
+    public void setAltiMinorVersion(int value)
+    {
+        minorVersion=value;
+    }
+    public void setAltiMajorVersion(int value)
+    {
+        majorVersion=value;
+    }
+
+
+
+    //altimeter baud rate
+    public void setConnectionSpeed(long value) {connectionSpeed = value;}
+    public long getConnectionSpeed() {return connectionSpeed;}
+
+    //Sensor resolution
+    public void setAltimeterResolution(int value) {altimeterResolution = value;}
+    public int getAltimeterResolution() {return altimeterResolution;}
+
+    //eeprom size
+    public void setEepromSize(int value) {eepromSize = value;}
+    public int getEepromSize() {return eepromSize;}
+
+
+
+    //index in an array
+    public int arrayIndex (String stringArray[], String pattern) {
+
+        for (int i =0; i < stringArray.length ; i++) {
+            if(stringArray[i].equals(pattern))
+                return i;
+        }
+        return -1;
+    }
+
+    //servos positions
+    public void setServoXMin(int value)
+    {
+        servoXMin = value;
+    }
+    public int getServoXMin()
+    {
+        return servoXMin;
+    }
+
+    public void setServoXMax(int value)
+    {
+        servoXMax = value;
+    }
+    public int getServoXMax()
+    {
+        return servoXMax;
+    }
+
+    public void setServoYMin(int value)
+    {
+        servoYMin = value;
+    }
+    public int getServoYMin()
+    {
+        return servoYMin;
+    }
+
+    public void setServoYMax(int value) { servoYMax = value; }
+    public int getServoYMax()
+    {
+        return servoYMax;
+    }
+
+    public void setUnits(int value)
+    {
+        units = value;
+    }
+
+    public int getUnits()
+    {
+        return units;
+    }
+
+    //beepingFrequency
+    public void setBeepingFrequency(int value)
+    {
+        beepingFrequency = value;
+    }
+    public int getBeepingFrequency()
+    {
+        return beepingFrequency;
+    }
+
+    //Minimum recording Altitude
+    public void setEndRecordAltitude(int value) {endRecordAltitude =value;}
+    public int getEndRecordAltitude(){return endRecordAltitude;}
+
+
+}

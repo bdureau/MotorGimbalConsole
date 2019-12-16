f6c7b656d3e2284656a6ca9b2dae8e62439a887b
BDUREAU <boris.dureau@neuf.fr>
2019-12-16 07:34:16 +0100

    Fixed orientation changes problems with the activity

--- app/src/main/java/com/motorgimbalconsole/FlightViewMPActivity.java
+++ app/src/main/java/com/motorgimbalconsole/FlightViewMPActivity.java
@@ -7,12 +7,13 @@ import android.content.Intent;
 
 import android.graphics.Typeface;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 import org.afree.data.xy.XYSeriesCollection;
-import org.afree.graphics.SolidColor;
+
 import org.afree.graphics.geom.Font;
 
 import android.graphics.Color;
@@ -45,16 +46,19 @@ public class FlightViewMPActivity extends AppCompatActivity {
     ConsoleApplication myBT;
     private FlightData myflight = null;
     private Button buttonDismiss, butSelectCurves, btnPlay;
-    String curvesNames[] = null;
-    boolean[] checkedItems =null;
+    private String curvesNames[] = null;
+    private String currentCurvesNames[] =null;
+    boolean[] checkedItems = null;
     XYSeriesCollection allFlightData;
     XYSeriesCollection flightData;
     ArrayList<ILineDataSet> dataSets;
-    //XYPlot plot;
-    //ValueAxis Xaxis;
-    //ValueAxis Yaxis;
+    int colors []= {Color.RED, Color.BLUE, Color.BLACK,
+            Color.GREEN, Color.CYAN, Color.GRAY, Color.MAGENTA, Color.YELLOW,Color.RED,
+            Color.BLUE, Color.BLACK,
+            Color.GREEN, Color.CYAN, Color.GRAY, Color.MAGENTA, Color.YELLOW, Color.RED, Color.BLUE, Color.BLACK,
+            Color.GREEN, Color.CYAN, Color.GRAY, Color.MAGENTA, Color.YELLOW};
     Font font;
-    //AFreeChart chart;
+
     private LineChart mChart;
     public static String SELECTED_FLIGHT = "MyFlight";
 
@@ -79,71 +83,63 @@ public class FlightViewMPActivity extends AppCompatActivity {
         font = new Font("Dialog", Typeface.NORMAL, fontSize);
 
         int numberOfCurves = flightData.getSeries().size();
-        String chartTitle= "";
+        String chartTitle = "";
 
         for (int i = 0; i < numberOfCurves; i++) {
 
-            if(i < (numberOfCurves -1) )
-                chartTitle = chartTitle  + flightData.getSeries(i).getKey().toString()+ "-";
+            if (i < (numberOfCurves - 1))
+                chartTitle = chartTitle + flightData.getSeries(i).getKey().toString() + "-";
             else
-                chartTitle = chartTitle  + flightData.getSeries(i).getKey().toString();
+                chartTitle = chartTitle + flightData.getSeries(i).getKey().toString();
         }
         //chart.setTitle(chartTitle);
 
         int graphBackColor;
         graphBackColor = myBT.getAppConf().ConvertColor(Integer.parseInt(myBT.getAppConf().getGraphBackColor()));
 
-        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
-        //chart.getTitle().setFont(font);
-        // set the background color for the chart...
-        //chart.setBackgroundPaintType(new SolidColor(graphBackColor));
 
-        // get a reference to the plot for further customisation...
-        /*plot = chart.getXYPlot();
-
-        plot.setDomainGridlinesVisible(false);
-        plot.setRangeGridlinesVisible(false);
-
-        plot.setBackgroundPaintType(new SolidColor(graphBackColor));
-        plot.setOutlinePaintType(new SolidColor(Color.YELLOW));
-        plot.setDomainZeroBaselinePaintType(new SolidColor(Color.GREEN));
-
-        plot.setRangeZeroBaselinePaintType(new SolidColor(Color.MAGENTA));*/
         int axisColor;
         axisColor = myBT.getAppConf().ConvertColor(Integer.parseInt(myBT.getAppConf().getGraphColor()));
 
         int labelColor = Color.BLACK;
 
         int nbrColor = Color.BLACK;
-        /*Xaxis = plot.getDomainAxis();
-        Xaxis.setAutoRange(true);
-        Xaxis.setAxisLinePaintType(new SolidColor(axisColor));
 
-        Yaxis = plot.getRangeAxis();
-
-        Yaxis.setAxisLinePaintType(new SolidColor(axisColor));
-
-
-        Xaxis.setTickLabelFont(font);
-        Xaxis.setLabelFont(font);
-
-        Yaxis.setTickLabelFont(font);
-        Yaxis.setLabelFont(font);
-
-        //Xaxis label color
-        Xaxis.setLabelPaintType(new SolidColor(labelColor));
-
-        Xaxis.setTickMarkPaintType(new SolidColor(axisColor));
-        Xaxis.setTickLabelPaintType(new SolidColor(nbrColor));
-        //Y axis label color
-        Yaxis.setLabelPaintType(new SolidColor(labelColor));
-        Yaxis.setTickLabelPaintType(new SolidColor(nbrColor));*/
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArray("CURRENT_CURVES_NAMES_KEY", currentCurvesNames);
+        outState.putBooleanArray("CHECKED_ITEMS_KEY",checkedItems);
+
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentCurvesNames = savedInstanceState.getStringArray("CURRENT_CURVES_NAMES_KEY");
+        checkedItems = savedInstanceState.getBooleanArray("CHECKED_ITEMS_KEY");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // recovering the instance state
+        if (savedInstanceState != null) {
+            currentCurvesNames = savedInstanceState.getStringArray("CURRENT_CURVES_NAMES_KEY");
+            checkedItems = savedInstanceState.getBooleanArray("CHECKED_ITEMS_KEY");
+        }
 
         //get the bluetooth Application pointer
         myBT = (ConsoleApplication) getApplication();
@@ -178,27 +174,58 @@ public class FlightViewMPActivity extends AppCompatActivity {
         myBT.getAppConf().ReadConfig();
 
 
-       drawGraph();
+        drawGraph();
 
-
-        mChart  = (LineChart) findViewById(R.id.linechart);
+        if (currentCurvesNames == null) {
+            //This is the first time so only display the altitude
+            currentCurvesNames = new String[curvesNames.length];
+            currentCurvesNames[0] ="altitude";
+            checkedItems = new boolean[curvesNames.length];
+            checkedItems[0] = true;
+        }
+        mChart = (LineChart) findViewById(R.id.linechart);
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(true);
-        int nbrData = flightData.getSeries(0).getItemCount();
+        /*int nbrData = flightData.getSeries(0).getItemCount();
 
-        ArrayList<Entry> yValues = new ArrayList <>();
+        ArrayList<Entry> yValues = new ArrayList<>();
 
         for (int i = 0; i < nbrData; i++) {
             yValues.add(new Entry(flightData.getSeries(0).getX(i).longValue(), flightData.getSeries(0).getY(i).longValue()));
+        }*/
+        dataSets = new ArrayList<>();
+        for (int i = 0; i < curvesNames.length; i++) {
+            if (checkedItems[i]) {
+                flightData.addSeries(allFlightData.getSeries(curvesNames[i]));
+
+                int nbrData = allFlightData.getSeries(i).getItemCount();
+
+                ArrayList<Entry> yValues = new ArrayList<>();
+
+                for (int k = 0; k < nbrData; k++) {
+                    yValues.add(new Entry(allFlightData.getSeries(i).getX(k).floatValue(), allFlightData.getSeries(i).getY(k).floatValue()));
+                }
+
+                LineDataSet set1 = new LineDataSet(yValues, "Altitude/Time");
+                set1.setColor(colors[i]);
+
+                set1.setDrawValues(false);
+                set1.setDrawCircles(false);
+                set1.setLabel(curvesNames[i]);
+
+                dataSets.add(set1);
+
+
+            }
         }
 
-        LineDataSet set1 = new LineDataSet(yValues, "Altitude/Time");
+        /*LineDataSet set1 = new LineDataSet(yValues, "Altitude/Time");
 
-
+        set1.setDrawCircles(false);
         set1.setDrawValues(false);
-        set1.setLabel("Altitude");
-        dataSets = new ArrayList<>();
-        dataSets.add(set1);
+        set1.setLabel("Altitude");*/
+        //dataSets = new ArrayList<>();
+        //dataSets.add(set1);
 
         LineData data = new LineData(dataSets);
         mChart.setData(data);
@@ -225,7 +252,8 @@ public class FlightViewMPActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int numberOfCurves = flightData.getSeries().size();
-                String currentCurvesNames[] = new String[numberOfCurves];
+                currentCurvesNames = new String[numberOfCurves];
+
                 for (int i = 0; i < numberOfCurves; i++) {
                     currentCurvesNames[i] = flightData.getSeries(i).getKey().toString();
                 }
@@ -235,10 +263,10 @@ public class FlightViewMPActivity extends AppCompatActivity {
                 checkedItems = new boolean[curvesNames.length];
                 // Add a checkbox list
                 for (int i = 0; i < curvesNames.length; i++) {
-                    if(Arrays.asList(currentCurvesNames).contains(curvesNames[i]))
+                    if (Arrays.asList(currentCurvesNames).contains(curvesNames[i]))
                         checkedItems[i] = true;
                     else
-                    checkedItems[i] = false;
+                        checkedItems[i] = false;
                 }
 
 
@@ -255,35 +283,32 @@ public class FlightViewMPActivity extends AppCompatActivity {
                         // The user clicked OK
                         drawGraph();
                         dataSets.clear();
+
                         flightData = new XYSeriesCollection();
                         for (int i = 0; i < curvesNames.length; i++) {
-                            if(checkedItems[i]) {
+                            if (checkedItems[i]) {
                                 flightData.addSeries(allFlightData.getSeries(curvesNames[i]));
-                               // for (int j =0; j< flightData.getSeries().size();j++) {
-                                    int nbrData = allFlightData.getSeries(i).getItemCount();
 
-                                    ArrayList<Entry> yValues = new ArrayList<>();
+                                int nbrData = allFlightData.getSeries(i).getItemCount();
 
-                                    for (int k = 0; k < nbrData; k++) {
-                                        yValues.add(new Entry(allFlightData.getSeries(i).getX(k).floatValue(), allFlightData.getSeries(i).getY(k).floatValue()));
-                                    }
+                                ArrayList<Entry> yValues = new ArrayList<>();
 
-                                    LineDataSet set1 = new LineDataSet(yValues, "Altitude/Time");
+                                for (int k = 0; k < nbrData; k++) {
+                                    yValues.add(new Entry(allFlightData.getSeries(i).getX(k).floatValue(), allFlightData.getSeries(i).getY(k).floatValue()));
+                                }
+
+                                LineDataSet set1 = new LineDataSet(yValues, "Altitude/Time");
+                                set1.setColor(colors[i]);
+
+                                set1.setDrawValues(false);
+                                set1.setDrawCircles(false);
+                                set1.setLabel(curvesNames[i]);
+
+                                dataSets.add(set1);
 
 
-                                    set1.setDrawValues(false);
-
-                                    set1.setLabel(curvesNames[i]);
-
-                                    dataSets.add(set1);
-                                //}
                             }
                         }
-                        //flightData.addSeries(allFlightData.getSeries("altitude"));
-
-                        //ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-                        //plot.setDataset(0, flightData);
-
 
                         LineData data = new LineData(dataSets);
                         mChart.clear();
